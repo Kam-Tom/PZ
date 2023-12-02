b30b54462c4ed0ceb029c8df74a2b2384ae4d5e5
MarcelHerman <herman-marcel@wp.pl>
2023-12-03 00:28:42 +0100

    ReCAPTCHA klienta
    ReCAPTCHA po stronie klienta - dorobić obsługę przycisków

--- dreamygadget.client/src/pages/LoginRegister.jsx
+++ dreamygadget.client/src/pages/LoginRegister.jsx
@@ -1,29 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "../css/LoginRegister.css"
+import ReCAPTCHA from "react-google-recaptcha";
+
 
 function LoginRegister() {
+    const ReCAPTCHA1 = useRef();
+    const ReCAPTCHA2 = useRef();
+
+    function checkCaptcha() {
+        const captchaValue = ReCAPTCHA1.current.getValue();
+        if (!captchaValue) {
+            console.log("nie weszlo");
+            confirm("Please verify the reCAPTCHA!");
+        } else {
+            // make form submission
+            console.log("weszlo");
+            alert("Form submission successful!");
+        }
+    }
+
+
     useEffect(() => {
         const container = document.getElementById('container');
         const registerBtn = document.getElementById('register');
         const loginBtn = document.getElementById('login');
+
+
         if (container && registerBtn && loginBtn) {
-          registerBtn.addEventListener('click', () => {
+            registerBtn.addEventListener('click', () => {
             container.classList.add("active");
           });
     
-          loginBtn.addEventListener('click', () => {
+            loginBtn.addEventListener('click', () => {
+                
             container.classList.remove("active");
           });
         }
         return () => {
           if (registerBtn) {
-            registerBtn.removeEventListener('click', () => {
+              registerBtn.removeEventListener('click', () => {
               container.classList.add("active");
             });
           }
     
           if (loginBtn) {
-            loginBtn.removeEventListener('click', () => {
+              loginBtn.removeEventListener('click', () => {
               container.classList.remove("active");
             });
           }
@@ -40,6 +61,7 @@ function LoginRegister() {
                     <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Password" />
                     <button>Sign Up</button>
+                    <ReCAPTCHA ref={ReCAPTCHA1} sitekey="6Lf7OCQpAAAAAJTm_KnO8VH5y-9p2wXztc1gSKkR" />
                 </form>
             </div>
             <div className="form-container sign-in">
@@ -49,6 +71,7 @@ function LoginRegister() {
                     <input type="password" placeholder="Password" />
                     <a href="#">Forget Your Password?</a>
                     <button>Sign In</button>
+                    <ReCAPTCHA ref={ReCAPTCHA2} sitekey="6Lf7OCQpAAAAAJTm_KnO8VH5y-9p2wXztc1gSKkR"/>
                 </form>
             </div>
             <div className="toggle-container">
