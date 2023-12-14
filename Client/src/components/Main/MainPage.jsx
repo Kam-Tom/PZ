1cded771a0d4db94c0f47f4e3a60726aeeacc0cb
MARFIK <tomaszewski.kacper1@tlen.pl>
2023-12-14 02:06:55 +0100

    Introduction to ProductTile, displaying a specific product page - tomorrow. Displaying tiles to improvement, everything is just a preview solution

--- Client/src/components/Main/MainPage.jsx
+++ Client/src/components/Main/MainPage.jsx
@@ -4,9 +4,27 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import LoginRegister from "../Account/LoginRegister";
 import AdminPage from "../Admin/AdminPage";
+import ProductTile from "./ProductTile";
+
+function TileArray(array, size) {
+    const tilesArray = [];
+    for (let i = 0; i < array.length; i += size) {
+        tilesArray.push(array.slice(i, i + size));
+    }
+    return tilesArray;
+}
 
 function MainPage() {
     const navigate = useNavigate();
+
+    const products = Array(12).fill({
+        name: 'iPhone 14 128GB 6.1"',
+        price: "4 099 zł",
+        stock: 7,
+        image: "https://pngimg.com/d/iphone_14_PNG6.png",
+    });
+
+    const productRows = TileArray(products, 4);
 
     return (
         <Routes>
@@ -16,6 +34,15 @@ function MainPage() {
                 element={
                     <>
                         <Navbar />
+                        <div className="product-list-container">
+                            {productRows.map((row, rowIndex) => (
+                                <div key={rowIndex} className="product-list-row">
+                                    {row.map((product, index) => (
+                                        <ProductTile key={index} product={product} />
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
                     </>
                 }
             />
