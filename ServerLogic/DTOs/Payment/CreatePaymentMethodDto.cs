55b9f3ce9ef7d56b1530c7e09ff54a7649e922f6
Kam-Tom <kamil8tomaszewski@gmail.com>
2023-12-16 20:42:21 +0100

    Create Payment

--- ServerLogic/DTOs/Payment/CreatePaymentMethodDto.cs
+++ ServerLogic/DTOs/Payment/CreatePaymentMethodDto.cs
@@ -10,6 +10,6 @@ namespace ServerLogic.DTOs.Payment;
 public class CreatePaymentMethodDto
 {
     public int Id { get; set; }
-    public PaymentMethodType type { get; set; }
+    public PaymentMethodType Method { get; set; }
     public int OrderId { get; set; }
 }
