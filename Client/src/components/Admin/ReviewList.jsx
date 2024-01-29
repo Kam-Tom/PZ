306dac3c17f26a11f0bf8fc0242be4564c73bebd
Kam-Tom <kamil8tomaszewski@gmail.com>
2024-01-29 22:00:45 +0100

    Added remove reviews from admin

--- Client/src/components/Admin/ReviewList.jsx
+++ Client/src/components/Admin/ReviewList.jsx
@@ -1,21 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import DataTable from './DataTable';
 import './DataTable.css';
-
+import { getAll, deleteElement } from '../../axios'
+import { useEffect } from "react";
 function ReviewList() {
     const renderReviewDetails = (data, onDelete) => {
-        return (
-            <div className="review-details">
-                <div className="review-info">
-                    <p>Email: {data.author}</p>
-                    <p>Rating: {data.rating}</p>
-                    <p>Comment: {data.comment}</p>
-                </div>
-                <div>
-                    <button className="delete-button" onClick={() => onDelete(data.id)}>Delete</button>
-                </div>
-            </div>
-        );
+        return <ProductReviews produtId={data.id} />
     };
 
     return (
@@ -27,6 +17,39 @@ function ReviewList() {
             ]}
             renderDetails={renderReviewDetails}
         />
+    );
+}
+function ProductReviews({ produtId }) {
+    const [reviews, setReviews] = useState([]);
+    async function fetchReviews() {
+        setReviews(await getAll(`https://localhost:7248/api/Review/${produtId}`));
+        console.log("DATA ", reviews)
+
+    }
+    async function deleteReview(review) {
+        await deleteElement(`https://localhost:7248/api/Review/Admin/${review.id}`);
+        setReviews((prevReviews) => prevReviews.filter((r) => r.id !== review.id));
+        console.log("DELETE ", review);
+    }
+    useEffect(() => {
+
+        fetchReviews();
+    }, []); 
+    return (
+        <>
+            {reviews.map((review) => (
+                <div className="review-details" key={review.id}>
+                    <div className="review-info">
+                        <p>Email: {review.author}</p>
+                        <p>Rating: {review.rating}</p>
+                        <p>Comment: {review.description}</p>
+                    </div>
+                    <div>
+                        <button className="delete-button" onClick={() => deleteReview(review)}>Delete</button>
+                    </div>
+                </div>
+            ))}
+        </>
     );
 }
 
